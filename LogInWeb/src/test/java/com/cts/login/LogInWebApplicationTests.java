1899d3556f766674c89f73fd0a8f946d1b0dc3b0
SLN <SLN@DESKTOP-GTKC1QB>
2020-08-29 20:14:25 +0530

    comitted

---
+++ LogInWeb/src/test/java/com/cts/login/LogInWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogInWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
